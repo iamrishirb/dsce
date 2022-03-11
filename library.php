c09303ec86c950edff3d0b1a7efea1178e77944f
iamrishirb <rishirbbajpai@outlook.com>
2022-03-11 17:19:27 +0530

    added library page

---
+++ library.php
@@ -0,0 +1,80 @@
+<?php
+    include 'header.php';
+?>
+
+<section class="hero-wrap hero-wrap-2" style="background-image: url('images/banner4.jpg');">
+    <div class="overlay"></div>
+    <div class="container">
+        <div class="row no-gutters slider-text align-items-center justify-content-center">
+            <div class="col-md-9 ftco-animate text-center">
+                <h1 class="mb-2 bread">College Library</h1>
+                <p class="breadcrumbs"><span class="mr-2"><a href="index.php">Home <i class="ion-ios-arrow-forward"></i></a></span> <span>College Library </span></p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- feature block -->
+<section class="ftco-services ftco-no-pb mt-4">
+    <div class="container">
+        <div class="row no-gutters">
+            <div class="col-md-4 d-flex services mt-4 align-self-stretch py-5 px-4 ftco-animate bg-primary">
+                <div class="media block-6 d-block text-center">
+                    <div class="media-body p-2 mt-3">
+                        <h1 style="font-weight: 800; font-size: 2 em; color: #fff;">4225</h1>
+                        <h3 class="heading">Books</h3>
+                        <p>We have a rich collection of books in our library from course books to literature and fiction books.</p></p>
+                    </div>
+                </div>      
+            </div>
+            <div class="col-md-4 d-flex services mt-4 align-self-stretch py-5 px-4 ftco-animate bg-darken">
+                <div class="media block-6 d-block text-center">
+                    <div class="media-body p-2 mt-3">
+                        <h1 style="font-weight: 800; font-size: 2 em; color: #fff;">08</h1>
+                        <h3 class="heading">Journals</h3>
+                        <p>We keep available the journals and important research papers for students to study.</p>
+                    </div>
+                </div>      
+            </div>
+            <div class="col-md-4 d-flex services mt-4 align-self-stretch py-5 px-4 ftco-animate bg-primary">
+                <div class="media block-6 d-block text-center">
+                    <div class="media-body p-2 mt-3">
+                        <h1 style="font-weight: 800; font-size: 2 em; color: #fff;">01</h1>
+                        <h3 class="heading">Encyclopedia</h3>
+                        <p>We have a vast Encyclopedia that the students can take help from to widen their knowledge.</p>
+                    </div>
+                </div>      
+            </div>
+            
+        </div>
+    </div>
+</section>
+
+<!-- feature points -->
+<section class="ftco-section ftco-no-pt ftc-no-pb py-5">
+    <div class="container">
+        <div class="row d-flex">
+            <div class="col-md-12 wrap-about py-5 pr-md-4 ftco-animate">
+                <h2 class="mb-4">Rules & Regulations</h2>
+                <p style="text-align: justify;">
+                The college has a rich library and reading room in which text books of all subjects are available.</p>
+
+                <p>✍︎ Students can get the book issued only after getting a library card made by depositing Rs 500 in the form of caution money.</p>
+
+                <p>✍︎ Receipt of admission fee and identity card is mandatory for making library card.</p>
+
+                <p>✍︎ A book is given to the students for a maximum period of 15 days.</p>
+
+                <p>✍︎ The second book can only be granted to a student, only on returning the first issued book.</p>
+
+                <p>✍︎ Penalty will be payable at the rate of Rs. 1 per day for late submission of book.
+
+                <p>✍︎ In case of loss or damage of the book, the full value of the book will have to be deposited by the student.</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php
+    include 'footer.php';
+?>
--- staff.php
+++ staff.php
@@ -16,24 +16,17 @@
 
 <section class="ftco-section bg-light">
     <div class="container px-4">
-        <h2 class="text-center">... to be updated</h2>
-        <!-- <div class="row">
+        <!-- <h2 class="text-center">... to be updated</h2> -->
+        <div class="row">
             <div class="col-md-6 col-lg-4 ftco-animate">
                 <div class="staff">
                     <div class="img-wrap d-flex align-items-stretch">
-                        <div class="img align-self-stretch" style="background-image: url(images/teacher-1.jpg);"></div>
+                        <div class="img align-self-stretch" style="background-image: url(images/bhoopendra.jpeg);"></div>
                     </div>
                     <div class="text pt-3 text-center">
-                        <h3>Bianca Wilson</h3>
-                        <span class="position mb-2">Teacher</span>
+                        <h3>Bhoopendra Singh Chauhan</h3>
+                        <span class="position mb-2">Principal</span>
                         <div class="faded">
-                            <p>I am an ambitious workaholic, but apart from that, pretty simple person.</p>
-                            <ul class="ftco-social text-center">
-                                <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
-                                <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
-                                <li class="ftco-animate"><a href="#"><span class="icon-google-plus"></span></a></li>
-                                <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
-                            </ul>
                         </div>
                     </div>
                 </div>
@@ -41,19 +34,12 @@
             <div class="col-md-6 col-lg-4 ftco-animate">
                 <div class="staff">
                     <div class="img-wrap d-flex align-items-stretch">
-                        <div class="img align-self-stretch" style="background-image: url(images/teacher-2.jpg);"></div>
+                        <div class="img align-self-stretch" style="background-image: url(images/himmat.jpeg);"></div>
                     </div>
                     <div class="text pt-3 text-center">
-                        <h3>Mitch Parker</h3>
-                        <span class="position mb-2">English Teacher</span>
+                        <h3>Himmat Bahadur Singh</h3>
+                        <span class="position mb-2">Assistant Professor</span>
                         <div class="faded">
-                            <p>I am an ambitious workaholic, but apart from that, pretty simple person.</p>
-                            <ul class="ftco-social text-center">
-                                <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
-                                <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
-                                <li class="ftco-animate"><a href="#"><span class="icon-google-plus"></span></a></li>
-                                <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
-                            </ul>
                         </div>
                     </div>
                 </div>
@@ -61,86 +47,17 @@
             <div class="col-md-6 col-lg-4 ftco-animate">
                 <div class="staff">
                     <div class="img-wrap d-flex align-items-stretch">
-                        <div class="img align-self-stretch" style="background-image: url(images/teacher-4.jpg);"></div>
+                        <div class="img align-self-stretch" style="background-image: url(images/shyam.jpeg);"></div>
                     </div>
                     <div class="text pt-3 text-center">
-                        <h3>Monshe Henderson</h3>
-                        <span class="position mb-2">Science Teacher</span>
+                        <h3>Shyam Sundar</h3>
+                        <span class="position mb-2">Assistant Professor</span>
                         <div class="faded">
-                            <p>I am an ambitious workaholic, but apart from that, pretty simple person.</p>
-                            <ul class="ftco-social text-center">
-                                <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
-                                <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
-                                <li class="ftco-animate"><a href="#"><span class="icon-google-plus"></span></a></li>
-                                <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
-                            </ul>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="row">
-            <div class="col-md-6 col-lg-4 ftco-animate">
-                <div class="staff">
-                    <div class="img-wrap d-flex align-items-stretch">
-                        <div class="img align-self-stretch" style="background-image: url(images/teacher-3.jpg);"></div>
-                    </div>
-                    <div class="text pt-3 text-center">
-                        <h3>Bianca Wilson</h3>
-                        <span class="position mb-2">Teacher</span>
-                        <div class="faded">
-                            <p>I am an ambitious workaholic, but apart from that, pretty simple person.</p>
-                            <ul class="ftco-social text-center">
-                                <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
-                                <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
-                                <li class="ftco-animate"><a href="#"><span class="icon-google-plus"></span></a></li>
-                                <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6 col-lg-4 ftco-animate">
-                <div class="staff">
-                    <div class="img-wrap d-flex align-items-stretch">
-                        <div class="img align-self-stretch" style="background-image: url(images/teacher-5.jpg);"></div>
-                    </div>
-                    <div class="text pt-3 text-center">
-                        <h3>Mitch Parker</h3>
-                        <span class="position mb-2">English Teacher</span>
-                        <div class="faded">
-                            <p>I am an ambitious workaholic, but apart from that, pretty simple person.</p>
-                            <ul class="ftco-social text-center">
-                                <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
-                                <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
-                                <li class="ftco-animate"><a href="#"><span class="icon-google-plus"></span></a></li>
-                                <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6 col-lg-4 ftco-animate">
-                <div class="staff">
-                    <div class="img-wrap d-flex align-items-stretch">
-                        <div class="img align-self-stretch" style="background-image: url(images/teacher-7.jpg);"></div>
-                    </div>
-                    <div class="text pt-3 text-center">
-                        <h3>Monshe Henderson</h3>
-                        <span class="position mb-2">Science Teacher</span>
-                        <div class="faded">
-                            <p>I am an ambitious workaholic, but apart from that, pretty simple person.</p>
-                            <ul class="ftco-social text-center">
-                                <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
-                                <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
-                                <li class="ftco-animate"><a href="#"><span class="icon-google-plus"></span></a></li>
-                                <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div> -->
     </div>
 </section>
 
